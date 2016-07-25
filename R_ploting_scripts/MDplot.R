6623691863b8e11cf67d67d8aefec0de1a763b8c
Jammyzx1 <james.l.mcdonagh@gmail.com>
2016-07-25 14:58:53 +0100

    An R script to automatically plot data
    Automatically plots the bond, angle and dihedral angle data

---
+++ R_ploting_scripts/MDplot.R
@@ -0,0 +1,58 @@
+MDplot <- function(path1){
+	count = 1
+	setwd(path1)
+	for(count in 1:3){
+		if(count==1) {
+			PathBonds <- paste(path1,"BONDS_plot",sep = "\\")
+			message(PathBonds)
+			setwd(PathBonds)
+		} else if(count==2) {
+			PathAngles <- paste(path1,"ANGLES_plot",sep = "\\")
+			message(PathAngles)
+			setwd(PathAngles)
+		} else if(count==3) {
+			PathD <- paste(path1,"DIHEDRAL_plot",sep = "\\")
+			message(PathD)
+			setwd(PathD)
+		} else {
+			stop("Counter past 3")
+		}
+		for(i in list.files(path=".")){
+			strin <- i
+			message(i)
+			title <- unlist(strsplit(strin, split="\\."))
+			if(count==1) {
+				fulltitle <- paste(title[1]," Bond Length")
+				lab_y <-"Bond Length (A)"
+				data <- try(read.table(i,sep=",",header=T),FALSE)
+				attach(data)
+				png(paste(title[1],".png"), width = 10, height = 7, units = 'in', res = 300)
+				plot(Geom, Value, main=fulltitle, xlab="Time Step (1000's)", ylab=lab_y) 
+				dev.off()
+				detach(data)
+			} else if(count==2) {
+				fulltitle <- paste(title[1]," Bond Angle")
+				lab_y <-"Bond Angle (degrees)"
+				data <- try(read.table(i,sep=",",header=T),FALSE)
+				attach(data)
+				png(paste(title[1],".png"), width = 10, height = 7, units = 'in', res = 300)
+				plot(Geom, Value, main=fulltitle, xlab="Time Step (1000's)", ylab=lab_y) 
+				dev.off()
+				detach(data)
+			} else if(count==3) {
+			fulltitle <- paste(title[1]," Dihedral Angle")
+				lab_y <-"Dihedral Angle (degrees)"
+				data <- try(read.table(i,sep=",",header=T),FALSE)
+				attach(data)
+				png(paste(title[1],".png"), width = 10, height = 7, units = 'in', res = 300)
+				plot(Geom, Positive_sense_angle, main=fulltitle, xlab="Time Step (1000's)", ylab=lab_y) 
+				dev.off()
+				detach(data)
+			}
+			
+		}
+		message("Finished set")
+		setwd(path1)
+	}
+	stop("FINISHED")
+}
